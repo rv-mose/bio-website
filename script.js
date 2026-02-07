07d5d3365de75f662ebaaf9fa7aac0575c65fcb9
rv-mose <appub4964@gmail.com>
2026-02-07 13:14:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,28 @@ document.querySelectorAll('.link').forEach(btn => {
     }, 150);
   });
 });
+document.getElementById("orderForm").addEventListener("submit", function(e){
+  e.preventDefault();
+  alert("Thank you! We will contact you on WhatsApp.");
+});
+document.getElementById("orderForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  let name = document.getElementById("name").value;
+  let phone = document.getElementById("phone").value;
+  let theme = document.getElementById("theme").value;
+  let links = document.getElementById("links").value;
+
+  let message = `Hi Arav,%0A
+I want an Instagram Bio Website 🚀%0A
+%0A
+Name: ${name}%0A
+Theme: ${theme}%0A
+Links: ${links}`;
+
+  let whatsappNumber = "918072646780"; // ← your number (no +)
+
+  let url = `https://wa.me/${whatsappNumber}?text=${message}`;
+
+  window.open(url, "_blank");
+});
