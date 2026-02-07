9d2175d38ad1789942928eaa2c76b1a7f11ba890
rv-mose <appub4964@gmail.com>
2026-02-07 11:34:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.link').forEach(btn => {
+  btn.addEventListener('click', () => {
+    btn.style.transform = 'scale(0.95)';
+    setTimeout(() => {
+      btn.style.transform = 'scale(1)';
+    }, 150);
+  });
+});
